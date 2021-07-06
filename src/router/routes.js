ad663fe078dd7c605502302f306619a1e783a5dc
Morteza Sabihi <mortezasabihi87@gmail.com>
2021-07-06 12:34:48 +0430

    feat: initial commit

---
+++ src/router/routes.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    path: "/",
+    name: "Home",
+    component: require("@/views/Home.vue").default,
+  },
+  // {
+  //   path: "/about",
+  //   name: "About",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  // },
+];
